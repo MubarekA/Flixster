1220f36f8611da92aeeb157c383cf6525414d608
mubareka <mubarekmabdela@gmail.com>
2020-10-28 15:51:56 -0400

    Flixster Part 2

--- app/src/main/java/com/example/flixster/models/Movie.java
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -3,14 +3,19 @@ package com.example.flixster.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+
+@Parcel
 public class Movie {
     String posterPath;
     String title;
     String overView;
+    double rating;
+    int movieId;
 
     // constructor takes in a json object and construct a movie object
     public Movie(JSONObject jsonObject) throws JSONException {
@@ -18,8 +23,12 @@ public class Movie {
         posterPath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overView = jsonObject.getString("overview");
+        rating = jsonObject.getDouble("vote_average");
+        movieId = jsonObject.getInt("id");
 
     }
+    //empty constructor by the parceler library
+    public Movie() {}
     public static List<Movie> fromJSONArray(JSONArray movieJsonArray) throws JSONException {
         List<Movie> movies = new ArrayList<>();
         for (int i = 0; i<movieJsonArray.length();i++) {
@@ -27,6 +36,7 @@ public class Movie {
         }
         return movies;
     }
+
 
     public String getPosterPath() {
         return String.format("https://image.tmdb.org/t/p/w500/%s",posterPath);
@@ -38,5 +48,13 @@ public class Movie {
 
     public String getOverView() {
         return overView;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public int getMovieId() {
+        return movieId;
     }
 }
